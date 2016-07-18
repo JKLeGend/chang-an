d7898a0f13af579f0e5deadb775223599c4d22e2
jukai.zhang <jukai.zhang@rea-group.com>
2016-07-18 21:47:25 +0800

    cake

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var gls = require('gulp-live-server');
+var browserSync = require('browser-sync').create(); // create a browser sync instance.
 
 gulp.task('sass', function () {
   return gulp.src('./src/main.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream());
 });
  
 
@@ -15,8 +17,27 @@ gulp.task('serve', function() {
   // var server = gls('./', true, 3030);
   server.start();
 
+  browserSync.init({
+    server: "./app"
+  });
+  gulp.watch("app/scss/*.scss", ['sass']);
+  gulp.watch("app/*.html").on('change', browserSync.reload);
+
   //use gulp.watch to trigger server actions(notify, start or stop)
   gulp.watch(['./src/*.scss', './index.html', './src/images/***'], ['sass'], function (file) {
     server.notify.apply(server, [file]);
+    //gulp.watch("scss/*.scss", ['sass']);
+    //gulp.watch("*.html").on('change', bs.reload);
   });
 });
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
+
+gulp.task('default', ['serve']);
+
